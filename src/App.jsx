87c917614970c3a23be4066146e9f9ec378bfca1
NicoMen <nicomendoza.92@gmail.com>
2021-09-18 17:53:36 +0200

    se hizo el transition router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { Switch, Route } from 'react-router-dom';
-import {Home} from './pages/home/Home'
-import {Nosotros} from './pages/nosotros/Nosotros'
-import {Productos} from './pages/productos/Productos'
-import {Contacto} from './pages/contacto/Contacto'
-import  Footer  from './componentes/footer/Footer';
+import { Home } from './pages/home/Home'
+import { Nosotros } from './pages/nosotros/Nosotros'
+import { Productos } from './pages/productos/Productos'
+import { Contacto } from './pages/contacto/Contacto'
+import Footer from './componentes/footer/Footer';
 
 function App() {
-  
+
   return (
-    
+
     <div className="footer-fix">
-      <Switch>
-            <Route path="/" exact>
-                <Home/>
-            </Route>
+      <Route render={({ location }) => (
+        <TransitionGroup>
+          <CSSTransition
+            key={location.key}
+            timeout={450}
+            classNames="page"
+          >
+            <Switch location={location}>
+              <Route path="/" exact>
+                <Home />
+              </Route>
 
-            <Route path="/productos">
-               <Productos />
-            </Route>
+              <Route path="/productos">
+                <Productos />
+              </Route>
 
-            <Route path="/nosotros">
+              <Route path="/nosotros">
                 <Nosotros />
-            </Route>
+              </Route>
 
-            <Route path="/contacto">
-                <Contacto/>
-            </Route>
-      </Switch>
-      <Footer/>
+              <Route path="/contacto">
+                <Contacto />
+              </Route>
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      )} />
+      <Footer />
     </div>
 
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './home.css';
-import { Card, Col, Container, Row } from 'react-bootstrap'
 import Header from '../../componentes/header/Header';
 
 
