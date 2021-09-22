35c141d40486407a39f4b1325e0c61f49fef3877
Rodrigo <rodrigomendozar10@gmail.com>
2021-09-22 12:44:07 -0300

    icon header

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,7 @@ function App() {
               <Route path="/contacto">
                 <Contacto />
               </Route>
+              
             </Switch>
           </CSSTransition>
         </TransitionGroup>
--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 // import NavbarRB from '../navbar/Navbar';
 import './header.css';
 
-import{faArrowCircleDown} from '@fortawesome/free-solid-svg-icons'
+import{faChevronCircleDown} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import NavbarRB from '../navbar/Navbar';
+import { NavLink } from 'react-router-dom';
 
 export default function Header({page, titulo, button, subtitulo}) {
 
@@ -17,9 +18,9 @@ export default function Header({page, titulo, button, subtitulo}) {
                <h4>{subtitulo}</h4> 
                 <button href="#" className="mt-5 boton-artesanal">{button}</button>
             </div>
-            <div>
-            <FontAwesomeIcon className="circleArrow" icon={faArrowCircleDown}/>
-            </div>
+            {/* <div  as={NavLink} to={`/${page}/container`}>
+            <FontAwesomeIcon className="circleArrow" icon={faChevronCircleDown}/>
+            </div> */}
       </div>
     </div>
   );
