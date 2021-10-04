293b521e9d77089ab5dfba4559b3692a266ab66a
NicoMen <nicomendoza.92@gmail.com>
2021-10-04 20:41:12 +0200

    se agrego esquema de busqeuda productos

--- src/pages/productos/Productos.jsx
+++ src/pages/productos/Productos.jsx
@@ -1,8 +1,9 @@
-
+import './productos.css';
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import Header from '../../componentes/header/Header'
 import { Producto } from '../../componentes/producto/Producto'
+import { Sidebar } from '../../componentes/producto/Sidebar'
 
 
 export const Productos = () => {
@@ -15,10 +16,16 @@ export const Productos = () => {
                 button="Todos nuestros CocoAlfajores"
             />
             <Container>
-            <Row className="mb-5">
-                  {/* hacer un map y traer los productos de producto */}
-                  <Producto/>
-            </Row>
+                <div className="row mt-5">
+                    <div className="col-md-4 mb-4">
+                        <Sidebar />
+                    </div>
+                    <div className="col-md-8 mb-4">
+                        <Producto />
+                    </div>
+                </div>
+
+
             </Container>
 
 
