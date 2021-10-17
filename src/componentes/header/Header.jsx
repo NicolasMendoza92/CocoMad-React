1d7b7d62da428706edafe8f27c7ca4bde9689db4
NicoMen <nicomendoza.92@gmail.com>
2021-10-17 18:44:41 +0200

    se puso boton scrolldown

--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 // import NavbarRB from '../navbar/Navbar';
 import './header.css';
 
-import{faChevronCircleDown} from '@fortawesome/free-solid-svg-icons'
+import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import NavbarRB from '../navbar/Navbar';
-import { NavLink } from 'react-router-dom';
 
-export default function Header({page, titulo, button, subtitulo}) {
+export default function Header({ page, titulo, button, subtitulo }) {
+
+  const scrollDown = () => {
+    window.scrollTo(0, 1000);
+  }
 
   return (
     <div className={`header-style background-${page}`}>
-      <NavbarRB/>
+      <NavbarRB />
       <div className="h-75 d-flex flex-column justify-content-between align-items-center">
-            <div className="my-5 text-center efecto-artesanal">
-               <h1>{titulo}</h1> 
-               <h4>{subtitulo}</h4> 
-                <button href="#" className="mt-5 boton-artesanal">{button}</button>
-            </div>
-            {/* <div  as={NavLink} to={`/${page}/container`}>
-            <FontAwesomeIcon className="circleArrow" icon={faChevronCircleDown}/>
-            </div> */}
+        <div className="my-5 text-center efecto-artesanal">
+          <h1>{titulo}</h1>
+          <h4>{subtitulo}</h4>
+          <button href="#" className="mt-5 boton-artesanal">{button}</button>
+        </div>
+        <botton className="boton-down" onClick={scrollDown}>
+          <FontAwesomeIcon className="circleArrow" icon={faChevronCircleDown} />
+        </botton>
       </div>
     </div>
   );
