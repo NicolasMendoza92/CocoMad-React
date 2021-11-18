1318d45e43fb4d95e07b7171e91ba3c4794c6cfc
NicoMen <nicomendoza.92@gmail.com>
2021-11-18 18:17:56 +0100

    mergeo log y register

--- src/componentes/login/FormLogin.jsx
+++ src/componentes/login/FormLogin.jsx
@@ -18,19 +18,19 @@ export const FormLogin = () => {
                 </div>
                 <Form>
                     <Form.Group className="mb-3" controlId="formGroupEmail">
-                        <Form.Label>Email</Form.Label>
+                        <Form.Label> <b>Email</b></Form.Label>
                         <Form.Control type="email" placeholder="Ingresa el correo" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupPassword">
-                        <Form.Label>Contraseña</Form.Label>
+                        <Form.Label> <b>Contraseña</b></Form.Label>
                         <Form.Control type="password" placeholder="Contraseña" />
                     </Form.Group>
                 </Form>
                 <div className="d-flex flex-column">
                     <button onClick={errorLink} type="submit" className="responsive-login-face"> <FaFacebookSquare className="mb-1" /> Iniciar sesión con facebook</button>
                     <div className="d-flex flex-column align-items-center justify-content-center crea-cuenta mt-2">
-                        <p className="mb-1">¿Aun no tienes cuenta?</p>
-                        <Link as={NavLink} to="/register">Crea una cuenta</Link>
+                        <p className="mb-1 text-black ">¿Aun no tienes cuenta?</p>
+                        <Link as={NavLink} to="/register">Create una!</Link>
                     </div>
                 </div>
             </Card.Body>
