7b49df633f311c00f7004e730fe74ee6d2b5bdbb
NicoMen <nicomendoza.92@gmail.com>
2021-11-20 12:02:31 +0100

    se trabajo en registerPage

--- src/componentes/register/FormRegister.jsx
+++ src/componentes/register/FormRegister.jsx
@@ -1,20 +1,74 @@
 import React from 'react'
+import swal from 'sweetalert'
+import axios from 'axios';
 import { useState } from 'react';
 import { Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 export const FormRegister = () => {
 
+    // validaciones de reactbootstrap
     const [validated, setValidated] = useState(false);
 
-    const handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
+    const [input, setInput] = useState({ name: '', lastName: '', email: '', password: '', knowcoco: '' });
+
+    const history = useHistory();
+
+    const handleChange = (e) => {
+        const { value, name } = e.target;
+        const newInput = { ...input, [name]: value };
+        if (newInput.name.length < 30
+            && newInput.lastName.length < 30
+            && newInput.email.length < 35
+            && newInput.password.length < 15) {
+            setInput(newInput);
+        } else {
+            swal('Alcanzaste el numero maximo de caracteres')
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:4000/api/auth/register', input);
+            swal({
+                title: "Excelente!",
+                text: "Te has registrado con exito!",
+                icon: "success",
+                button: "Continua"
+            });
+            history.push('/login');
+        } catch (error) {
+            console.error(error);
+            if (input.name === '' && input.lastName === '' && input.email === '' && input.password === '') {
+                swal("Faltan datos", "Completar los campos obligatorios", "warning")
+            }
+            else if (input.name === '') {
+                swal('completa el nombre')
+            }
+            else if (input.lastName === '') {
+                swal('completa el apellido')
+            }
+            else if (input.email === '') {
+                swal('completa el email')
+            }
+            else if (input.password === '') {
+                swal('completa la contraseña')
+            }
+            else if (input.knowcoco === '') {
+                swal('completa este campo')
+            }
+            else {
+                swal(JSON.stringify(error.response.data));
+            }
+
         }
 
         setValidated(true);
+        if (setValidated === true) {
+            e.target.reset();
+        }
+
     };
 
 
@@ -23,13 +77,24 @@ export const FormRegister = () => {
             noValidate validated={validated}
             className="form-register px-3 my-5" onSubmit={handleSubmit}>
             <Form.Group className="mb-3 row align-items-center justify-content-center" controlId="formBasicName">
-                <label className="col-11 col-md-6 text-center">Nombre y Apellido</label>
+                <label className="col-11 col-md-6 text-center">Nombre </label>
                 <Form.Control
                     className="col-11 col-md-9 "
                     name="name"
-                    // onChange={(e) => handleChange(e)}
+                    onChange={(e) => handleChange(e)}
                     type="text"
-                    maxLength="20"
+                    maxLength="30"
+                    required
+                />
+            </Form.Group>
+            <Form.Group className="mb-3 row align-items-center justify-content-center" controlId="formBasiclastName">
+                <label className="col-11 col-md-6 text-center">Apellido</label>
+                <Form.Control
+                    className="col-11 col-md-9 "
+                    name="lastName"
+                    onChange={(e) => handleChange(e)}
+                    type="text"
+                    maxLength="30"
                     required
                 />
             </Form.Group>
@@ -38,7 +103,7 @@ export const FormRegister = () => {
                 <Form.Control
                     className="col-11 col-md-9"
                     name="email"
-                    // onChange={(e) => handleChange(e)}
+                    onChange={(e) => handleChange(e)}
                     type="email"
                     maxLength="35"
                     required
@@ -49,7 +114,7 @@ export const FormRegister = () => {
                 <Form.Control
                     className="col-11 col-md-9"
                     name="password"
-                    // onChange={(e) => handleChange(e)}
+                    onChange={(e) => handleChange(e)}
                     type="password"
                     minLength="6"
                     maxLength="15"
@@ -61,9 +126,9 @@ export const FormRegister = () => {
                 <Form.Select
                     className="col-11 col-md-9 text-center"
                     name="knowcoco"
-                    //  onChange={(e) => handleChange(e)}
+                    onChange={(e) => handleChange(e)}
                     required>
-                    <option value="" disabled selected >Elje una opcion</option>
+                    <option value="" disabled selected>Elije una opcion</option>
                     <option value="si">Si</option>
                     <option value="no">No</option>
                     <option value="ire">No, pero quiero ir!</option>
